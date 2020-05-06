3e3d6273ef7b114bc4c457e18d4a8d20f92277fa
mus90 <mustafaalamin.07@gmail.com>
2020-05-06 23:20:18 +0200

    Adding ListGroup from react-bootstrap and defining default todos array in the state

--- Projects/ToDoList/to-do-list/src/components/todos/TodoList.js
+++ Projects/ToDoList/to-do-list/src/components/todos/TodoList.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import TodoListItem from "./TodoListItem";
+//import TodoListItem from "./TodoListItem";
 import NewTodoForm from "./NewTodoForm";
+import { ListGroup } from "react-bootstrap";
 
 export default class TodoList extends Component {
-  state = [{ todo: "" }];
+  state = {
+    todo: "",
+    todos: [
+      {
+        text: "wake up",
+        done: false,
+      },
+    ],
+  };
 
   setInputHandler = (event) => {
     this.setState({ todo: event.target.value });
@@ -12,7 +21,13 @@ export default class TodoList extends Component {
     return (
       <div className="list-wrapper">
         <NewTodoForm changed={this.setInputHandler} />
-        <TodoListItem onChange={this.state.todo} />
+
+        <ListGroup>
+          <ListGroup.Item>{this.state.todos[0].text} </ListGroup.Item>
+          <ListGroup.Item>{this.state.todo} </ListGroup.Item>
+          <ListGroup.Item />
+          <ListGroup.Item />
+        </ListGroup>
       </div>
     );
   }
